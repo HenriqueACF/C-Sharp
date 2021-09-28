75fcf235bb821b02b7a22dc72c9b5deac5e173e3
henrique <henrique.assis145@gmail.com>
2021-09-28 10:00:08 -0300

    up

---
+++ iniciando-c#/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace iniciando_c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeVezes = 5;
+
+            for(int i = 0; i < numeroDeVezes; i++){
+                Console.WriteLine($"teste {i}");
+            }
+        }
+    }
+}
