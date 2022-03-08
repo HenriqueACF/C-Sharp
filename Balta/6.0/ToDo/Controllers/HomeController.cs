e894ac80318f6c2d2dc7260230256142451688e1
henriqueacf <henrique.assis145@gmail.com>
2022-03-08 09:59:22 -0300

    add controllers

---
+++ Balta/6.0/ToDo/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDo.Controllers
+{
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("/")]
+        public string Get()
+        {
+            return "hello";
+        }
+    }
+    
+}
+
--- Balta/6.0/ToDo/Data/AppDbContext.cs
+++ Balta/6.0/ToDo/Data/AppDbContext.cs
@@ -1,12 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 using Todo.Models;
 
-namespace ToDo.Data;
-
-public class AppDbContext : DbContext
+namespace ToDo.Data
 {
-    public DbSet<TodoModels> Todos { get; set; }
+    public class AppDbContext : DbContext
+    {
+        public DbSet<TodoModels> Todos { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-        => options.UseSqlite("DataSource=app.db;Cache=shared");
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite("DataSource=app.db;Cache=shared");
+    }
+    
 }
