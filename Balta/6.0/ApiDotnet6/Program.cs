4c873823a3a655ee1c2f266910a187b006dcef0b
henriqueacf <henrique.assis145@gmail.com>
2022-03-07 21:53:21 -0300

    presentation api dotnet6

--- Balta/6.0/ApiDotnet6/Program.cs
+++ Balta/6.0/ApiDotnet6/Program.cs
@@ -2,5 +2,20 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/{nome}", (string nome) =>
+{
+    return Results.Ok($"Hello {nome}");
+});
+
+app.MapPost("/", (User user) =>
+{
+    return Results.Ok(user);
+});
 
 app.Run();
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+}
