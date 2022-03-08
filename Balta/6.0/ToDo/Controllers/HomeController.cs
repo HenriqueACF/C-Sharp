4b199857769de7d2467ee1e241aced55ab987a6c
henriqueacf <henrique.assis145@gmail.com>
2022-03-08 10:55:05 -0300

    create methods get post put and delete

--- Balta/6.0/ToDo/Controllers/HomeController.cs
+++ Balta/6.0/ToDo/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using ToDo.Data;
+using Todo.Models;
 
 namespace ToDo.Controllers
 {
@@ -6,9 +8,59 @@ namespace ToDo.Controllers
     public class HomeController : ControllerBase
     {
         [HttpGet("/")]
-        public string Get()
+        public List<TodoModels> Get([FromServices]AppDbContext context)
         {
-            return "hello";
+            return context.Todos.ToList();
+        }
+        
+        [HttpGet("/{id:int}")]
+        public TodoModels GetById(
+            [FromRoute] int id,
+            [FromServices]AppDbContext context)
+        {
+            return context.Todos.FirstOrDefault(x => x.Id == id);
+        }
+        
+        [HttpPost("/")]
+        public TodoModels Post(
+            [FromBody]TodoModels todo,
+            [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return todo;
+        }
+        
+        [HttpPut("/")]
+        public TodoModels Put(
+            [FromRoute]int id,
+            [FromBody]TodoModels todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+            if (model == null)
+                return todo;
+
+            model.Title = todo.Title;
+            model.Done = todo.Done;
+
+            context.Todos.Update(model);
+            context.SaveChanges();
+            return model;
+        }
+        
+        [HttpDelete("/")]
+        public TodoModels Delete(
+            [FromRoute]int id,
+            [FromBody]TodoModels todo,
+            [FromServices] AppDbContext context)
+        {
+            var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+            context.Todos.Remove(model);
+            context.SaveChanges();
+            return model;
         }
     }
     
