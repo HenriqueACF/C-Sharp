9e42fcc16db212e6ae561b19079df91a7d6f12f4
henriqueacf <henrique.assis145@gmail.com>
2022-03-10 11:35:22 -0300

    create EditorCategoryViewModel

--- Balta/6.0/blog/Controllers/CategoryController.cs
+++ Balta/6.0/blog/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 using Blog.Data;
 using Blog.Models;
+using blog.ViewModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -42,15 +43,21 @@ namespace Blog.Controllers
         
         [HttpPost("v1/categories")]
         public async Task<IActionResult> PostAsync(
-            [FromBody] Category model,
+            [FromBody] EditorCategoryViewModel model,
             [FromServices] BlogDataContext context)
         {
             try
             {
-                await context.Categories.AddAsync(model);
+                var category = new Category
+                {
+                    Id = 0,
+                    Name = model.Name,
+                    Slug = model.Slug.ToLower()
+                };
+                await context.Categories.AddAsync(category);
                 await context.SaveChangesAsync();
 
-                return Created($"v1/categories/{model.Id}", model);
+                return Created($"v1/categories/{category.Id}", category);
             }
             catch (DbUpdateException ex)
             {
@@ -65,7 +72,7 @@ namespace Blog.Controllers
         [HttpPut("v1/categories/{id:int}")]
         public async Task<IActionResult> PutAsync(
             [FromRoute] int id,
-            [FromBody] Category model,
+            [FromBody] EditorCategoryViewModel model,
             [FromServices] BlogDataContext context)
         {
             try
---
+++ Balta/6.0/blog/ViewModel/EditorCategoryViewModel.cs
@@ -0,0 +1,7 @@
+namespace blog.ViewModel;
+
+public class EditorCategoryViewModel
+{
+    public string Name { get; set; }
+    public string Slug { get; set; }
+}
