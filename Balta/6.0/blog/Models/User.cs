720c5e64b8590b82d5b0d1209164655e69ebf91c
henriqueacf <henrique.assis145@gmail.com>
2022-03-14 18:06:49 -0300

    feat: add pagination

--- Balta/6.0/blog/Models/User.cs
+++ Balta/6.0/blog/Models/User.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace blog.Models
 {
     public class User
@@ -5,6 +7,8 @@ namespace blog.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
+        
+        [JsonIgnore]
         public string PasswordHash { get; set; }
         public string Image { get; set; }
         public string Slug { get; set; }
