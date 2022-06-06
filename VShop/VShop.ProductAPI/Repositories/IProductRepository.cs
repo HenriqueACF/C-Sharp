b97994e8e883ec869f0d6b02ca4f915d84bb1f71
henriqueacf <henrique.assis145@gmail.com>
2022-06-06 11:23:55 -0300

    feat: [Repository] -> create interface and repositories by product and category

---
+++ VShop/VShop.ProductAPI/Repositories/IProductRepository.cs
@@ -0,0 +1,12 @@
+using VShop.ProductAPI.Models;
+
+namespace VShop.ProductAPI.Repositories;
+
+public interface IProductRepository
+{
+    Task<IEnumerable<Product>> GetAll();
+    Task<Product> GetById(int id);
+    Task<Product> Create(Product product);
+    Task<Product> Update(Product product);
+    Task<Product> Delete(int id);
+}
