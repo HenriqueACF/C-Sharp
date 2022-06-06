3fe89a3ace3e5ada87f719206417efcab4bee549
Henrique Assis <45006908+HenriqueACF@users.noreply.github.com>
2022-06-05 21:55:47 -0300

    remove project

--- VShop/VShop.ProductAPI/Properties/Models/Category.cs
+++
@@ -1,8 +0,0 @@
-namespace VShop.ProductAPI.Properties.Models;
-
-public class Category
-{
-    public int CategoryId { get; set; }
-    public string? Name { get; set; }
-    public ICollection<Product>? Products { get; set; }
-}
