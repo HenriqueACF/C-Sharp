6b5de42d9a9a2cbe2bec1ae58553221fe7bb5c8b
henriqueacf <henrique.assis145@gmail.com>
2022-06-09 09:49:01 -0300

    feat: add swagger

--- MinimalApi/MinimalApi/Program.cs
+++ MinimalApi/MinimalApi/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//SWAGGER
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseSwagger();
 
 app.MapGet("/", () => "Hello World!");
 
+app.UseSwaggerUI();
+
 app.Run();
+
+public record Cliente(string Nome, string Email);
