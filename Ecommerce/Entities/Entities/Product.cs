274a318c496955caa2b37c439f2f2af141c866de
henriqueacf <123segurana>
2022-06-22 15:51:07 -0300

    feat: init entities basic classes

---
+++ Ecommerce/Entities/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Entities.Notifications;
+
+namespace Entities.Entities;
+
+    [Table("Product")]
+    public class Product: Notifies
+    {
+        [Column("PRD_ID")]
+        [Display(Name = "Codigo")]
+        public int Id { get; set; }
+        
+        [Column("PRD_NAME")]
+        [Display(Name = "Nome")]
+        public int Name { get; set; }
+        
+        [Column("PRD_VALUE")]
+        [Display(Name = "Valor")]
+        public decimal Value { get; set; }
+        
+        [Column("PRD_ESTADO")]
+        [Display(Name = "Estado")]
+        public bool Estado { get; set; }
+    }
