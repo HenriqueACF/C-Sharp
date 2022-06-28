03ba65857a8ff3545a4251a576d8df6194539421
henriqueacf <123segurana>
2022-06-28 17:04:23 -0300

    add DB

---
+++ FilmesApi/FilmesApi/Controllers/FilmeController.cs
@@ -0,0 +1,41 @@
+﻿using FilmesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesApi.Controllers;
+
+[ApiController]
+[Route("Controller")]
+public class FilmeController: ControllerBase
+{
+    private static List<Filme> filmes = new List<Filme>();
+    private static int id = 1;
+    
+    [HttpPost]
+    public IActionResult AdicionarFilme([FromBody]Filme filme)
+    {
+        filme.Id = id++;
+        filmes.Add(filme);
+        return
+            CreatedAtAction(nameof(RecuperarFilmePorId), 
+                new { Id = filme.Id }, 
+                filme);
+    }
+
+    [HttpGet]
+    public IActionResult RecuperarFilmes()
+    {
+        return Ok(filmes);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult RecuperarFilmePorId(int id)
+    {
+        Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+        if (filme is not null)
+        {
+            return Ok(filme);
+        }
+
+        return NotFound();
+    }
+}
---
+++ FilmesApi/FilmesApi/Models/Filme.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesApi.Models;
+
+public class Filme
+{
+    public int Id { get; set; }
+    [Required(ErrorMessage = "O Titulo precisa ser informado")]
+    public string Titulo { get; set; }
+    [Required (ErrorMessage = "O diretor precisa ser informado")]
+    public string Diretor { get; set; }
+    public string Genero { get; set; }
+    [Range(1,600, ErrorMessage = "Aduração precisa ser informada")]
+    public int Duracao { get; set; }
+}
