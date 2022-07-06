797608825b0286e8446172ecbfc3f986aae157d3
henriqueacf <123segurana>
2022-07-06 15:23:41 -0300

    feat: [usuarioApi] -> add packages

---
+++ FilmesApi/UsuariosApi/Controllers/CadastroController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.DTOs;
+
+namespace UsuariosApi.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class CadastroController: ControllerBase
+{
+    [HttpPost]
+    public IActionResult Cadastrarusuario(CreateUsuarioDto createDto)
+    {
+        return Ok();
+    }
+}
---
+++ FilmesApi/UsuariosApi/Program.cs
@@ -0,0 +1,36 @@
+using System.Configuration;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using UsuariosApi.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddDbContext<UserDBContext>(
+    options => options.UseMySQL(builder.Configuration
+        .GetConnectionString("UsuarioConnection")));
+builder.Services.AddIdentity<IdentityUser<int>, IdentityRole<int>>()
+    .AddEntityFrameworkStores<UserDBContext>();
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
