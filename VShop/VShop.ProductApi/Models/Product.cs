f1e1268ee0b686c0822598af27ec54fad2db8d20
henriqueacf <henrique.assis145@gmail.com>
2022-07-09 18:08:51 -0300

    init project

---
+++ VShop/VShop.ProductApi/Models/Product.cs
@@ -0,0 +1,11 @@
+namespace VShop.ProductApi.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Description { get; set; }
+    public long Stock { get; set; }
+    public string? ImageURL { get; set; }
+}
