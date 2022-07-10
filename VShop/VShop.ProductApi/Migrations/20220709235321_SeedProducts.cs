0111ec636364819720e5d8e950fea54896c9cc7d
henriqueacf <henrique.assis145@gmail.com>
2022-07-09 21:04:13 -0300

    feat: create tables with migrations

---
+++ VShop/VShop.ProductApi/Migrations/20220709235321_SeedProducts.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductApi.Migrations
+{
+    public partial class SeedProducts : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into Products" +
+                   "(Name," +
+                   " Price," +
+                   " Description," +
+                   " Stock," +
+                   " ImageURL," +
+                   " CategoryId)" +
+                   " VALUES(" +
+                   "'Caderno'," +
+                   " 7.55," +
+                   "'Caderno'," +
+                   " 10," +
+                   "'Caderno.jpg'" +
+                   ",1)");
+            
+            mb.Sql("Insert into Products" +
+                   "(Name," +
+                   " Price," +
+                   " Description," +
+                   " Stock," +
+                   " ImageURL," +
+                   " CategoryId)" +
+                   " VALUES(" +
+                   "'Lapis'," +
+                   " 8.5," +
+                   "'Lapis'," +
+                   " 100," +
+                   "'Lapis.jpg'" +
+                   ",1)");
+            
+            mb.Sql("Insert into Products" +
+                   "(Name," +
+                   " Price," +
+                   " Description," +
+                   " Stock," +
+                   " ImageURL," +
+                   " CategoryId)" +
+                   " VALUES(" +
+                   "'Clips'," +
+                   " 1.8," +
+                   "'Clips para papel'," +
+                   " 200," +
+                   "'clips.jpg'" +
+                   ",2)");
+        }
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from Products");
+        }
+    }
+}
