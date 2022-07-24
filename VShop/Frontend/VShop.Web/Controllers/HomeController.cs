f2df0ab4b617d11a575ba568edbd99e6ef49a9f9
henriqueacf <henrique.assis145@gmail.com>
2022-07-24 20:21:00 -0300

    feat:[authentication] -> add authentication to aplication

--- VShop/Frontend/VShop.Web/Controllers/HomeController.cs
+++ VShop/Frontend/VShop.Web/Controllers/HomeController.cs
@@ -1,26 +1,57 @@
-﻿using System.Diagnostics;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
 
 namespace VShop.Web.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    private readonly IProductService _productService;
+    public HomeController(ILogger<HomeController> logger, 
+        IProductService productService )
     {
         _logger = logger;
+        _productService = productService;
     }
 
-    public IActionResult Index()
+    public async Task<IActionResult> Index()
     {
-        return View();
+        var products = await _productService.GetAllProducts(string.Empty);
+
+        if (products is null)
+            return View("Error");
+
+        return View(products);
     }
-    
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+
+    [HttpGet]
+    public async Task<ActionResult<ProductVM>> ProductDetails(int id)
     {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        var product = await _productService.FindProductById(id,string.Empty);
+
+        if (product is null)
+            return View("Error");
+
+        return View(product);
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error(string message)
+    {
+        return View(new ErrorViewModel { ErrorMessage = message });
+    }
+
+    [Authorize]
+    public async Task<IActionResult> Login()
+    {
+        var accessToken = await HttpContext.GetTokenAsync("access_token");
+        return RedirectToAction(nameof(Index));
+    }
+    public IActionResult Logout()
+    {
+        return SignOut("Cookies", "oidc");
     }
 }
