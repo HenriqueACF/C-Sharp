3b237994ec0267d78634b3a40b38ef48a01a7dab
henriqueacf <henrique.assis145@gmail.com>
2022-08-01 11:38:43 -0300

    feat:[homecontroller] -> add product detailpost

--- VShop/Frontend/VShop.Web/Controllers/HomeController.cs
+++ VShop/Frontend/VShop.Web/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using VShop.Web.Models;
+using VShop.Web.Services;
 using VShop.Web.Services.Contracts;
 
 namespace VShop.Web.Controllers;
@@ -10,11 +11,13 @@ public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
     private readonly IProductService _productService;
+    private readonly ICartService _cartService;
     public HomeController(ILogger<HomeController> logger, 
-        IProductService productService )
+        IProductService productService, ICartService cartService)
     {
         _logger = logger;
         _productService = productService;
+        _cartService = cartService;
     }
 
     public async Task<IActionResult> Index()
@@ -28,14 +31,48 @@ public class HomeController : Controller
     }
 
     [HttpGet]
+    [Authorize]
     public async Task<ActionResult<ProductVM>> ProductDetails(int id)
     {
-        var product = await _productService.FindProductById(id,string.Empty);
+        var token = await HttpContext.GetTokenAsync("access_token");
+        var product = await _productService.FindProductById(id,token);
     
         if (product is null)
             return View("Error");
     
         return View(product);
+    }
+
+    [HttpPost]
+    [ActionName("ProductDetails")]
+    [Authorize]
+    public async Task<ActionResult<ProductVM>> ProductDetailsPost(ProductVM productVM)
+    {
+        var token = await HttpContext.GetTokenAsync("access_token");
+        CartVM cart = new()
+        {
+            CartHeader = new CartHeaderVM
+            {
+                UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value
+            }
+        };
+        
+        CartItemVM cartItem = new()
+        {
+            Quantity = productVM.Quantity,
+            ProductId = productVM.Id,
+            Product = await _productService.FindProductById(productVM.Id, token)
+        };
+
+        List<CartItemVM> cartItemsVM = new List<CartItemVM>();
+        cartItemsVM.Add(cartItem);
+        cart.CartItems = cartItemsVM;
+
+        var result = await _cartService.AddItemToCartAsync(cart, token);
+
+        if (result is not  null)
+            return RedirectToAction(nameof(Index));
+        return View(productVM);
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
